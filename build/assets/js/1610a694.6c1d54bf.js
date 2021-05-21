09526e78c0dd2d351122836b43ad7b16eda603b7
liugoldent <liugoldent@gmail.com>
2021-05-21 19:05:34 +0800

    clear commit

--- build/assets/js/1610a694.6c1d54bf.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkcovidnews=self.webpackChunkcovidnews||[]).push([[989],{9165:function(s){"use strict";s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"covid-19","name":"covid-19","count":1,"permalink":"/blog/tags/covid-19"}')}}]);
